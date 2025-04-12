05d2e139bacd44cc2ba1a68ff3de823798fc0143
cyberhuda <hhassoun@umich.edu>
2025-04-11 20:32:57 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,75 +1,7 @@
-// var video;
-
-// window.addEventListener("load", function() {
-// 	console.log("Good job opening the window")
-//     video = document.querySelector("#player1");
-// 	video.autoplay = false;
-// 	video.loop = false;
-// 	console.log("this works");
-
-//     document.querySelector("#play").addEventListener("click", function() {
-// 		console.log("Play Video");
-// 		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
-// 		video.play();
-// 	});
-
-// // document.querySelector("#play").addEventListener("click", function() {
-// // 	console.log("Play Video");
-// // });
-
-// 	document.querySelector("#pause").addEventListener("click", function() {
-// 		console.log("Pause Video");
-// 		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
-// 		video.pause();
-// 	});
-
-// 	document.querySelector("#slower").addEventListener("click", function() {
-// 		console.log("Slow Down Video");
-// 		video.playbackRate *= 0.90;
-// 		console.log("New speed is " + video.playbackRate);
-// 	});
-// 	document.querySelector("#faster").addEventListener("click", function() {
-// 		console.log("Speed Up Video");
-// 		video.playbackRate /= 0.90;
-// 		console.log("New speed is " + video.playbackRate);
-// 	});
-// 	document.querySelector("#mute").addEventListener("click", function() {
-// 		if (video.muted==false){
-// 		video.muted = true;
-// 		console.log("Mute Video");
-// 		}
-// 		else{
-// 		video.muted = false;
-// 		console.log("Unmute Video");
-// 		}
-// 	});
-// 	document.querySelector("#slider").addEventListener("click", function() {
-// 		console.log("Changing volume");
-// 		video.volume = this.value / 100;
-// 		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
-// 	});
-// 	document.querySelector("#skip").addEventListener("click", function() {
-// 		console.log("Skip Ahead");
-// 		if (video.currentTime + 15 > video.duration) {
-// 			video.currentTime = 0;
-// 		} else {
-// 			video.currentTime += 15;
-// 		}
-// 		console.log("New time is " + video.currentTime);
-// 	});
-// 	document.querySelector("#vintage").addEventListener("click", function() {
-// 		console.log("Changing CSS");
-// 		video.classList.add("oldSchool");
-// 	});
-// 	document.querySelector("#orig").addEventListener("click", function() {
-// 		console.log("Changing CSS");
-// 		video.classList.remove("oldSchool");
-// 	});
-// });
-
-
 var video;
-
+// This format is from https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+// This is the preferred way to add event listeners for the load event.
 window.addEventListener("load", function() {
     console.log("Good job opening the window");
     video = document.querySelector("#player1");
@@ -77,7 +9,7 @@ window.addEventListener("load", function() {
     video.loop = false;
     console.log("Auto play and loop are set to false");
 
-    // Initialize volume display, this line was supposed to stay uncommented
+    // Initialize volume display, this line was supposed to stay uncommented. From discussion
     // document.querySelector("#volume").textContent = (video.volume * 100) + "%";
 
     document.querySelector("#play").addEventListener("click", function() {
@@ -90,6 +22,7 @@ window.addEventListener("load", function() {
         console.log("Pause Video");
         video.pause();
     });
+    // These 3 code format is from https://www.tutorialspoint.com/how-to-change-video-playing-speed-using-javascript
 
     document.querySelector("#slower").addEventListener("click", function() {
         video.playbackRate *= 0.9;
@@ -109,7 +42,8 @@ window.addEventListener("load", function() {
         }
         console.log("Current location: " + video.currentTime);
     });
-
+    // I worked on this with table mates in discussion. We used information from She Codes
+    // https://www.shecodes.io/athena/91747-what-does-the-document-queryselector-function-do-in-javascript
     document.querySelector("#mute").addEventListener("click", function() {
         console.log("Mute Video");
         if (video.muted == true) {
@@ -121,6 +55,8 @@ window.addEventListener("load", function() {
         }
     });
 
+    // These next 3 are from stack overflow and using the console in the example video to test
+    // https://stackoverflow.com/questions/19700193/how-to-get-the-volume-of-html5-video
     document.querySelector("#slider").addEventListener("input", function() {
         video.volume = this.value / 100;
         document.querySelector("#volume").textContent = (video.volume * 100) + "%";
